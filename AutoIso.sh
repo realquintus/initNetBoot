7fcfdeb684916a8cca748ea7523e252fc26e5ae7
realquintus <pablohousse@gmail.com>
2019-12-17 01:33:09 +0100

    Amélioration de AutoIso, il génére maintenant une commande live-build valide.

--- AutoIso.sh
+++ AutoIso.sh
@@ -1,27 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Bienvenue dans l'interface de création d'image iso live personnalisées, AutoIso"
+echo "Bienvenue dans l'interface de personalisation d'image live Debian, AutoDebIso"
 set -e
 commande="lb config noauto "
 ##	Architecture	##
-while [ $bits -ne 32 ] || [ $bits -ne 64 ];do
-	echo -e "Voulez-vous utiliser une image 32 ou 64 bits, 64 par défaut ? (32/64)\n"
+echo -e "Voulez-vous utiliser une image 32 ou 64 bits, 64 par défaut ? (32/64)"
+read bits
+bits=${bits:-64}
+while true;do
+	if [ $bits -eq 32 ] 2> /dev/null || [ $bits -eq 64 ] 2> /dev/null;then
+		break
+	fi
+	echo "Entrez 32 ou 64:"
 	read bits
-	${bits:-64}
 done
 if [ $bits -eq 32 ];then
 	commande=$commande"--architecture i386 --linux-flavours \"586 686-pae\" "
 elif [ $bits -eq 64 ];then
-	commande=$commande"--architecture amd64"	
+	commande=$commande"--architecture amd64 "	
 fi;
 ##########################
 ##	Image Linux	##
-echo -e "Quel iso Linux voulez-vous utiliser?\n"
-while [[ $image == *.iso ]];do
-	echo "Entrez une image au format .iso :" 
+echo -e "Quel version de Debian voulez-vous utiliser voulez-vous utiliser? (A appeler par son petit nom)"
+read version
+version=${version:-"buster"}
+while true;do
+	if [[ -n $(echo "buster,stretch,jessie,wheezy,squeeze,lenny,etch" | grep -io $version ) ]] 2> /dev/null;then
+		commande=$commande"--distribution $version "
+		break
+	fi
+	echo "Entrez une version valide de Debian (Buster, Stretch, Jessie, Wheezy, Squeeze, Lenny ou Etch):" 
 	read image
 done;
-	commande=$commande"--linux"
+##########################
+commande=$commande"--linux-packages \"linux-image\" --ignore-system-defaults"
+echo "Entrez le nom de l'utilisateur (test par défault):"
+read username
+username=${username:-"test"}
+commande=$commande"bootappend-live \"boot=live components autologin username=$username lang=fr_FR.UTF-8 locales=fr_FR.UTF-8 keyboard-layouts=fr keyboard-model=pc105 timezone=Europe/Paris utc=yes toram swap=true\""
+echo $commande
 ##########################
 #lb config noauto \
 #    --architectures i386 \
