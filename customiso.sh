e4f122e105a43937d3ae2928d0f28eebdadb7ccc
pablo <pablohousse@gmail.com>
2020-02-01 15:52:17 +0100

    Customiso.sh : Ajout de commentaires et fonction usage

--- customiso.sh
+++ customiso.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
-$pathiso=$0"/newiso"
-if [[ $1 = *.iso ]] && [ -e $1 ];then
+##########################################################################################
+#	Customiso.sh allows, as its name suggests, to modify and customize iso's easily  #
+##########################################################################################
+usage(){
+	echo -e "Customiso.sh allows, as its name suggests, to modify and customize iso easily\nYou can enter a iso file as first argument"
+}
+pathiso=$(echo $0 | sed 's/customiso.sh//g')"newiso"
+if [[ $1 = *.iso ]] 2> /dev/null && [ -e $1 ] 2> /dev/null;then
 	image=$1
 else
 	echo "Entrez une imagei iso à personnaliser:"
@@ -14,8 +19,14 @@ else
 		read image
 	done
 fi
-sudo mkdir /mnt/tempiso
+mkdir /mnt/tempiso || fail=1
+echo $fail
+if [ $fail -eq 1 ] 2> /dev/null;then
+	echo "Vous ne disposez pas des droits suffisants" 
+	exit 1	
+fi
 mkdir $pathiso
 mount -o loop $image /mnt/tempiso
-cp /mnt/tempiso/* $pathiso
-unmount /mnt/tempiso
+cp -r /mnt/tempiso/* $pathiso
+umount /mnt/tempiso
+rm -rf /mnt/tempiso
