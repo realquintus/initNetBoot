0d9d3230ebd1348471b7980a7e36a1d5df01fbec
pablo <pablohousse@gmail.com>
2020-02-04 13:05:35 +0100

    Amelioration et debugage initNetBoot.sh

--- initNetBoot/initNetBoot.sh
+++ initNetBoot/initNetBoot.sh
@@ -1,32 +1,50 @@
 #!/bin/bash
 
-while getopts "f:" option;do
+while getopts "f:v" option;do
 	case $option in
 		f)
 			if [ -e $OPTARG ];then
+				image=$OPTARG
+			else
 				echo -e "Error, file does not exist"
 				exit 1
 			fi
-			image=$OPTARG	
+		;;
+		v)
+			verb="true"
+		;;
 	esac
 done
-#################### Installing/updating dependencies #############################################################
-lib=$(ls /var/lib/)												  #
-[ -n $(echo $lib | grep apt) ] || install_command="apt install -y dnsmasq tftpd syslinux-common isc-dhcp-server"  #
-[ -n $(echo $lib | grep pacman) ] || install_command="pacman -Sy --noconfirm dnsmasq tftp-hpa syslinux dhcp"	  #
-[ -n $(echo $lib | grep yum) ] || install_command="yum install -y dnsmasq tftp-server syslinux dhcp-server"	  #
-###################################################################################################################
-
+# Checking if server is using UEFI, if not, it might cause problem like break your PC
+[ -d /sys/firmware/efi ] && efi="true" || efi="false"
+#################### Installing/updating dependencies ##########################################################################
+lib=$(ls /var/lib/)												  	       #
+[ -z $(echo $lib | grep apt) ] 2> /dev/null || install_command="apt install -y dnsmasq syslinux-common"  #
+[ -z $(echo $lib | grep pacman) ] 2> /dev/null || install_command="pacman -Sy --noconfirm dnsmasq syslinux"      #
+[ -z $(echo $lib | grep yum) ] 2> /dev/null || install_command="yum install -y dnsmasq syslinux"       #
+if [[ $verb = "true" ]];then												       #
+	echo $install_command												       #
+fi															       #
+eval $install_command													       #
+################################################################################################################################
 # Creating tftp folder
-sudo mkdir -p /var/tftpboot/pxelinux.cfg/
+if [[ $verb = "true" ]];then
+	echo "Creating tftp's file"
+fi
+sudo mkdir -p /var/lib/tftpboot/pxelinux.cfg/
 # Creating boot menu
-sudo cp $(echo $0 | sed 's/initNetBoot.sh//g')/pxelinux_menu.txt /var/tftpboot/pxelinux.cfg/default 
+if [[ $verb = "true" ]];then
+	echo "Loading boot menu in "
+fi
+sudo cp $(echo $0 | sed 's/initNetBoot.sh//g')pxelinux_menu.txt /var/lib/tftpboot/pxelinux.cfg/default 
 
-################ Get needed library and put it in tftp folder ###########################################
-cd /													#
-path_lib=$(locate pxelinux.0 | grep -E /pxelinux.0$ | sed 's/\/pxelinux.0//g' )				#
-cp $(echo $path_lib)/{pxelinux.0,vesamenu.c32,ldlinux.c32,libcom32.c32,libutil.c32} /var/lib/tftpboot/	#
-#########################################################################################################
+################ Get needed library and put it in tftp folder #####################################################
+cd /														  #
+path_lib=$(locate -e pxelinux.0 | grep -E /pxelinux.0$ | sed 's/\/pxelinux.0//g' )				  #
+if [ -z $(ls /var/lib/tftpboot/ | grep pxelinux.0) ];then							  #
+	cp -r $(echo $path_lib)/{pxelinux.0,vesamenu.c32,ldlinux.c32,libcom32.c32,libutil.c32} /var/lib/tftpboot/ #
+fi														  #
+###################################################################################################################
 
 #cd /var/lib/tftpboot/
 #sudo cp /usr/lib/PXELINUX/pxelinux.cfg .
@@ -34,7 +52,7 @@ cp $(echo $path_lib)/{pxelinux.0,vesamenu.c32,ldlinux.c32,libcom32.c32,libutil.c
 #sudo cp /usr/lib/syslinux/modules/bios/* .
 #sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.old
 
-################ Make the image available on the network ########
-file_exten=$(echo $image | cut -d "." -f2)			#
-sudo cp $image /var/tftpboot/netboot_image.$(echo $file_exten)  #
-#################################################################
+################ Make the image available on the network ############
+file_exten=$(echo $image | awk -F "." '{print $NF}')		    #
+sudo cp $image /var/lib/tftpboot/netboot_image.$(echo $file_exten)  #
+#####################################################################
