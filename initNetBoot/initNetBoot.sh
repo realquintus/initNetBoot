a876a277c2e77a37fd4e59856d4f1f606588d3eb
pablo <pablohousse@gmail.com>
2020-03-08 17:50:19 +0100

    Correction de bugs

--- initNetBoot/initNetBoot.sh
+++ initNetBoot/initNetBoot.sh
@@ -2,41 +2,47 @@
 usage(){
 	echo -e "\e[1mInitNetBoot.sh\e[0m is a bash script that is used to initialize a network boot server using PXE.\nFirstly, it will install/update dependencies, then configure and enable DHCP and TFTP server.\n\t\e[1m\e[4mOptions:\e[0m\e[0m\n\t\t-h : Show this help message.\n\t\t-f : This option is needed and is used to indicate the image that you want to be available on the local network.\n\t\t-I : This option is required and is used to indicates the network interface through which the server will be available.\n\t\t-v : Verbose mode"
 }
-
-while getopts "f:vI:h" option;do
-	case $option in
-		f)
-			if [ -e $OPTARG ];then
-				image=$OPTARG
-			else
-				echo -e "Error, file does not exist"
-				usage
-				exit 1
-			fi
-		;;
-		v)
-			verb="true"
-		;;
-		I)	
-			ip a s $OPTARG 2> /dev/null > /dev/null
-			if [ $? -eq 0 ];then	
-				interface=$OPTARG
-				srv_ip=$(ifconfig $interface | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sed -n 1p)
-				if [ -z $srv_ip ];then
-					echo "The specified network interface does not have an IP address."
-					exit 3
-				fi
-			else
-				echo "The specified network interface does not exist"
-				exit 2
-			fi
-		;;
-		h)
-			usage
-			exit 0
-		;;
-	esac
-done
+########################################## Handling of options ##########################################################
+while getopts "f:vI:h" option;do											#
+	case $option in													#
+		f)													#
+			if [ -e $OPTARG ];then										#
+				image=$OPTARG										#
+			else												#
+				echo -e "Error, file does not exist"							#
+				usage											#
+				exit 1											#
+			fi												#
+		;;													#
+		v)													#
+			verb="true"											#
+		;;													#
+		I)													#
+			ip a s $OPTARG 2> /dev/null > /dev/null								#
+			if [ $? -eq 0 ];then										#
+				interface=$OPTARG									#
+				srv_ip=$(ifconfig $interface | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sed -n 1p)	#
+					if ! [[ -z $srv_ip ]];then
+						if [[ $verb = "true" ]];then
+							echo "Flushing ip address on $interface"
+						fi
+						ip addr flush dev $interface;
+					fi										#
+					ip addr add 192.168.55.254/24 dev $interface
+					#echo "The specified network interface does not have an IP address."		#
+					#exit 3										#						
+			else												#
+				echo "The specified network interface does not exist"					#
+				exit 2											#
+			fi												#
+		;;													#
+		h)													#
+			usage												#
+			exit 0												#
+		;;													#
+	esac														#
+done															#
+#########################################################################################################################
 
 # Checking if every needed informations are specified
 if [ -z $image ] || [ -z $interface ] || [ -z $srv_ip ];then
@@ -45,33 +51,41 @@ if [ -z $image ] || [ -z $interface ] || [ -z $srv_ip ];then
 	exit 4
 fi
 
-# Checking if server is using UEFI, if not, it might cause problem like break your PC
+# Checking if server is using UEFI, if not, it might cause problem like break your bios
 [ -d /sys/firmware/efi ] && efi="true" || efi="false"
 
-#################### Installing/updating dependencies ##########################################################################
-lib=$(ls /var/lib/)												  	       #
-[ -z $(echo $lib | grep apt) ] 2> /dev/null || install_command="apt install -y $(if [[ $verb = "true" ]];then;echo "-q";fi)dnsmasq syslinux-common"  		       #
-[ -z $(echo $lib | grep pacman) ] 2> /dev/null || install_command="pacman -Sy  $(if [[ $verb = "true" ]];then;echo "-q";fi) --noconfirm dnsmasq syslinux"      	       #
-[ -z $(echo $lib | grep yum) ] 2> /dev/null || install_command="yum install -y $(if [[ $verb = "true" ]];then;echo "-q";fi) dnsmasq syslinux"       			       #
-if [[ $verb = "true" ]];then												       #
-	echo "Installing needed packages: "$install_command										       #
-else
-	install_command="$install_command"" > /dev/null"
+#################### Installing/updating dependencies #####################################################################################################
+lib="$(ls /var/lib/)"												  	       				  #
+if ! [ -z $(echo $lib | grep "apt") ];then
+	install_command="apt install -y ""$(if [ -z $verb ];then echo '-q ';fi)""dnsmasq syslinux-common"  	  #
 fi
-eval $install_command													       #
-################################################################################################################################
+if ! [ -z $(echo $lib | grep "pacman") ];then
+	install_command="pacman -Sy $(if [ -z $verb ];then echo '-q ';fi)--noconfirm dnsmasq syslinux" #
+fi
+if ! [ -z $(echo $lib | grep "yum") ];then
+	install_command="yum install -y $(if [ -z $verb ];then echo '-q ';fi)dnsmasq syslinux"
+fi
+if [[ $verb = "true" ]];then												       				  #
+	echo "Installing needed packages: "$install_command										      		  #
+else																			  #
+	install_command="$install_command"" > /dev/null"												  #
+fi																			  #
+eval $install_command													       				  #
+###########################################################################################################################################################
 
-# Creating tftp folder
-if [[ $verb = "true" ]];then
-	echo "Creating tftp's file"
-fi
-sudo mkdir -p /var/lib/tftpboot/pxelinux.cfg/
+######## Creating tftp folder #################
+if [[ $verb = "true" ]];then		      #
+	echo "Creating tftp's folder"	      #
+fi					      #
+sudo mkdir -p /var/lib/tftpboot/pxelinux.cfg/ #
+###############################################
 
-# Creating boot menu
-if [[ $verb = "true" ]];then
-	echo "Loading boot menu in "
-fi
-sudo cp $(echo $0 | sed 's/initNetBoot.sh//g')pxelinux_menu.txt /var/lib/tftpboot/pxelinux.cfg/default 
+###################### Creating boot menu ###############################################################
+if [[ $verb = "true" ]];then										#
+	echo "Loading boot menu in /var/lib/tftpboot/pxelinux.cfg"					#
+fi													#
+sudo cp $(echo $0 | sed 's/initNetBoot.sh//g')pxelinux_menu.txt /var/lib/tftpboot/pxelinux.cfg/default 	#
+#########################################################################################################
 
 ################ Get needed library and put it in tftp folder #####################################################
 														  #
@@ -84,17 +98,18 @@ if [ -z $(ls /var/lib/tftpboot/ | grep pxelinux.0) ];then							  #
 fi														  #
 ###################################################################################################################
 
-cd $(locate initNetBoot.sh | sed 's/initNetBoot.sh//g')
+#cd $(echo $0 | sed 's/initNetBoot.sh//g')
 ################ Make the image available on the network ############
 if [[ $verb = "true" ]];then					    #
 	echo "Copying netboot image on tftp folder..."		    #
 fi								    #
-file_exten=$(echo $image | awk -F "." '{print $NF}')		    #								    #
+file_exten=$(echo $image | awk -F "." '{print $NF}')		    #
 sudo cp $image /var/lib/tftpboot/netboot_image.$(echo $file_exten)  #
 #####################################################################
 
-### Configure dnsmasq ###
-if [[ $verb = "true" ]];then
-	echo "Configuring dnsmasq..."
-fi
-cat dnsmasq.conf | sed "s/%NIC%/$interface/g" | sed "s/%srv_addr%/$srv_ip/g" > /etc/dnsmasq.conf
+########################### Configure dnsmasq ####################################################
+if [[ $verb = "true" ]];then									 #
+	echo "Configuring dnsmasq..."								 #
+fi												 #
+cat $(echo $0 | sed 's/initNetBoot.sh//g')dnsmasq.conf | sed "s/%NIC%/$interface/g" | sed "s/%srv_addr%/$srv_ip/g" > /etc/dnsmasq.conf #
+##################################################################################################
