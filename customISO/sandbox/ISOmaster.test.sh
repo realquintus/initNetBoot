296fcc231b2a40b4419e3bed913ede854a7e5a0a
root <root@debian>
2020-06-06 13:08:50 +0200

    sandbox iso

---
+++ customISO/sandbox/ISOmaster.test.sh
@@ -0,0 +1,34 @@
+### Test de présence du paquet live-build
+#test=$(lb --version)
+#if [ -n "$test" ]; then 
+	#echo -e "Live-Build installé en version $(echo $test) \n"
+#else 
+	#echo -e "Live-Build n'est pas installé, l'installation va démarrer \n "
+	#sudo apt install live-build
+#fi
+
+#sudo mkdir $HOME/live-debian
+#sudo cd $HOME/live-debian
+#echo -e "Le script va construite l'image ISO dans le répertoire $(echo$(pwd))"
+#sudo lb config
+ubuntu="0"
+debian="0"
+echo -e "L'initialisation de la construction de l'image ISO est en cours, \n des choix sur la personnalisation de celle-ci vont vous être proposés. \n 
+Veuillez répondre correctement. "
+echo -e "Quel distribution de Linux ? \t [debian - ubuntu]"
+read dist
+while [ -z $dist ] | [ $dist != "debian" ] | [ $dist != "ubuntu" ]; do
+	if [ $(echo $dist) = "debian" ]; then 
+		debian="1"
+		break
+	elif [ $(echo $dist) = "ubuntu" ]; then
+       		ubuntu="1"
+		break	
+	else
+		echo "Veuillez remplir correctement le champ indiqué"
+		read dist
+		continue
+	fi
+done
+echo -e "debian : $debian"
+echo -e "ubuntu : $ubuntu"
---
+++ customISO/sandbox/test.sh
@@ -0,0 +1,6 @@
+dist="debian"
+ubuntu=0
+if [ $(echo $dist) = "debian" ];then 
+	ubuntu=1
+	echo $ubuntu
+      fi	
