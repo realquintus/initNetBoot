40d35a8d3935b4758e80e7050834ce6314ded700
root <root@debian>
2020-06-06 11:59:06 +0200

    script iso

--- customISO/ISOmaster.sh
+++ customISO/ISOmaster.sh
@@ -1,4 +1,17 @@
-sudo apt install live-manual live-build
+### Test de présence du paquet live-build
+test=$(lb --version)
+if [ -n "$test" ]; then 
+	echo -e "Live-Build installé en version $(echo $test) \n"
+else 
+	echo -e "Live-Build n'est pas installé, l'installation va démarrer \n "
+	sudo apt install live-build
+fi
+
 sudo mkdir $HOME/live-debian
 sudo cd $HOME/live-debian
+echo -e "Le script va construite l'image ISO dans le répertoire $(echo$(pwd))"
 sudo lb config
+echo -e "L'initialisation de la construction de l'image ISO est en cours, \n des choix sur la personnalisation de celle-ci vont vous être proposés. \n 
+Veuillez répondre correctement. "
+echo -e "Quel distribution de Linux ? \t [debian - ubuntu]"
+read
