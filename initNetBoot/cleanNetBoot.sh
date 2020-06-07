cfc40871c49ad83d433726c1d7adec645c5dd9b0
pablo <pablohousse@gmail.com>
2020-06-07 17:42:57 +0200

    correction de bugs

--- initNetBoot/cleanNetBoot.sh
+++ initNetBoot/cleanNetBoot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 usage(){
-	echo -e "cleanNetBoot is a bash script that revert the initNetBoot script."
+	echo -e "\e[1mCleanNetBoot\e[0m is a bash script that revert the initNetBoot script.\n\t\e[1m\e[4mOptions:\e[0m\e[0m\n\t\t-h : Show this help message.\n\t\t-v : Verbose mode.\n\t\t-F : \"Full clean\" this option is used to entirely remove PXE including IP address, packages and tftp folder.\n\t\t-n : \"no confirm\" this option is used to avoid confirm messages."
 }
 ####### Handling options ################
 while getopts "Fnhv" option;do
@@ -24,11 +24,11 @@ done
 
 ####### Configuring network interface ####
 NIC=$(ip a s | grep "192.168.9.254/24" | awk '{print $NF}' | grep -E "*")
-if [ $? -ne 1 ] && [[ -n $full_clean ]];then
+if [ $? -ne 1 ] && [[ $full_clean == "true" ]];then
 	if [ -z $no_confirm ];then
 		echo "Do you want to flush $NIC IP address (Y/n):"
 		read confirm_NIC
-		${confirm_NIC-"y"}
+		confirm_NIC=${confirm_NIC:-"y"}
 	fi
 	if [ -n $no_confirm ] || [[ $confirm_NIC == "Y" ]] || [[ $confirm_NIC == "y" ]];then
 		ip a f dev $NIC
@@ -40,7 +40,7 @@ fi
 #########################################
 
 ######## removing dependencies ##########
-if [ -n $full_clean ];then
+if [[ $full_clean == "true" ]];then
 	lib=$(ls /var/lib)
 	if ! [ -z $(echo $lib | grep -o "apt") ];then
 		remove_command="apt remove -y ""$(if [ -z $verb ];then echo '-q ';fi)""dnsmasq pxelinux syslinux-common nfs-kernel-server"
@@ -54,7 +54,7 @@ if [ -n $full_clean ];then
 	if [ -z $no_confirm ];then
 		echo "Do you want to remove dnsmasq, pxelinux,syslinux-common, nfs-utils (Y/n):"
 		read confirm_packages
-		${confirm_packages-"y"}
+		confirm_packages=${confirm_packages:-"y"}
 	fi
 	if [ -z $no_confirm ] || [[ $confirm_packages == "y" ]] || [[ $confirm_packages == "Y" ]];then
 		if [[ $verb = "true" ]];then
@@ -66,6 +66,7 @@ if [ -n $full_clean ];then
 		if [[ $verb = "true" ]];then
 			echo -e "## Remove finished ##\n"
 		fi
+	fi
 fi
 ######################################
 
@@ -73,7 +74,7 @@ fi
 if [ -z $no_confirm ];then
 	echo "Do you want to clean tftp folder, if you entered full clean option it will delete all of it. If not it will simply delete the current image (Y/n):"
 	read confirm_folder
-	${confirm_folder-"y"}
+	confirm_folder=${confirm_folder:-"y"}
 fi
 if [ -z $no_confirm ] || [[ $confirm_folder == "y" ]] || [[ $confirm_folder == "Y" ]];then
 	if [[ $verb = "true" ]];then		      #
