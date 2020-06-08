a282f48c0a2b427e9f6e80df10238e0a694b8c3d
solenb <solenbellouati@gmail.com>
2020-06-08 16:42:04 +0200

    08/05/20

--- customISO/sandbox/isomaster.test.sh
+++ customISO/sandbox/isomaster.test.sh
@@ -10,8 +10,8 @@ fi
 
 
 ###################  Création du répertoire de travail  ############################################# 
-sudo mkdir -p $HOME/live-debian
-cd $HOME/live-debian
+#sudo mkdir -p $HOME/live-debian
+#cd $HOME/live-debian
 echo -e "Le script va construite l'image ISO dans le répertoire $HOME/live-debian"
 #sudo lb config
 
@@ -21,7 +21,7 @@ debian=0
 dist="null"
 echo -e "L'initialisation de la construction de l'image ISO est en cours, \n des choix sur la personnalisation de celle-ci vont vous être proposés. \n 
 Veuillez répondre correctement. "
-echo -e "Quel distribution de Linux ? \t [debian - ubuntu]"
+echo -e "Quel distribution de Linux voulez-vous ? \t [debian - ubuntu]"
 
 while [ $debian -ne 1 ] | [ $ubuntu -ne 1 ]; do
 	if [[ $dist == "debian" ]]; then 
@@ -39,5 +39,54 @@ while [ $debian -ne 1 ] | [ $ubuntu -ne 1 ]; do
 	fi
 done
 
+#### Ubuntu environment metapackages ####
+gnome_u=0				#
+kde_u=0					#
+mate_u=0				#
+cinna=0					#
+#########################################
 
+#### Debian environment metapackages ####
+gnome_d=0				#
+kde_d=0					#
+mate_d=0				#
+#########################################
+
+envi="null"
+if [ $debian -eq 1 ]; then
+	echo -e "Quel envirronement graphique Debian voulez-vous ? \t [gnome - kde - mate]"
+else
+	echo -e "Quel envirronement graphique Ubuntu voulez-vous ? \t [gnome - kde - mate - cinnamon]"
+fi
+
+while [ $gnome_u -ne 1 ] | [ $kde_u -ne 1 ] | [ $mate_u -ne 1 ] | [ $cinna -ne 1 ] | [ $gnome_d -ne 1 ] | [ $kde_d -ne 1 ] | [ $mate_d -ne 1 ]; do
+	if [[ $envi == "gnome_u" ]]; then 
+		gnome_u=1
+		break
+	elif [[ $envi == "kde_u" ]]; then
+       		kde_u=1
+		break		
+	elif [[ $envi == "mate_u" ]]; then
+       		mate_u=1
+		break			
+	elif [[ $envi == "cinna" ]]; then
+       		cinna=1
+		break	
+	elif [[ $envi == "gnome_d" ]]; then
+       		gnome_d=1
+		break	
+	elif [[ $envi == "kde_d" ]]; then
+       		kde_d=1
+		break	
+	elif [[ $envi == "mate_d" ]]; then
+       		mate_d=1
+		break	
+	else
+		if [[ $envi != "null" ]];then
+			echo "Veuillez remplir correctement le champ indiqué"
+		fi
+		read dist
+		continue
+	fi
+done
 
