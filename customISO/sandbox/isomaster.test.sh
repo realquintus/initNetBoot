7c5348f4a56aa375f994ca9afad38f35346c0ac1
solenb <solenbellouati@gmail.com>
2020-06-08 16:51:05 +0200

    08/05/20 + desktop_env

--- customISO/sandbox/isomaster.test.sh
+++ customISO/sandbox/isomaster.test.sh
@@ -1,24 +1,28 @@
+#! /bin/bash
+
 ###################  Test de présence du paquet live-build  #########################################
-test=$(lb --version)
-if [ -n "$test" ]; then 
-	echo -e "Live-Build installé en version $(echo $test)"
-else 
-	echo -e "Live-Build n'est pas installé, l'installation va démarrer \n "
-	sudo apt install live-build
-fi
+test=$(lb --version)										    #				
+if [ -n "$test" ]; then 									    #
+	echo -e "Live-Build installé en version $(echo $test)"					    #
+else 												    #
+	echo -e "Live-Build n'est pas installé, l'installation va démarrer \n "			    #
+	sudo apt install live-build								    #
+fi												    #
 #####################################################################################################
 
 
 ###################  Création du répertoire de travail  ############################################# 
-#sudo mkdir -p $HOME/live-debian
-#cd $HOME/live-debian
-echo -e "Le script va construite l'image ISO dans le répertoire $HOME/live-debian"
-#sudo lb config
-
+#sudo mkdir -p $HOME/live-debian								    #
+#cd $HOME/live-debian										    #
+echo -e "Le script va construite l'image ISO dans le répertoire $HOME/live-debian"		    #
+#sudo lb config											    #
 #####################################################################################################
+
+
 ubuntu=0
 debian=0
 dist="null"
+
 echo -e "L'initialisation de la construction de l'image ISO est en cours, \n des choix sur la personnalisation de celle-ci vont vous être proposés. \n 
 Veuillez répondre correctement. "
 echo -e "Quel distribution de Linux voulez-vous ? \t [debian - ubuntu]"
@@ -43,7 +47,7 @@ done
 gnome_u=0				#
 kde_u=0					#
 mate_u=0				#
-cinna=0					#
+cinnamon=0					#
 #########################################
 
 #### Debian environment metapackages ####
@@ -54,12 +58,12 @@ mate_d=0				#
 
 envi="null"
 if [ $debian -eq 1 ]; then
-	echo -e "Quel envirronement graphique Debian voulez-vous ? \t [gnome - kde - mate]"
+	echo -e "Quel envirronement graphique Debian voulez-vous ? \t [gnome_u - kde_u - mate_u]"
 else
-	echo -e "Quel envirronement graphique Ubuntu voulez-vous ? \t [gnome - kde - mate - cinnamon]"
+	echo -e "Quel envirronement graphique Ubuntu voulez-vous ? \t [gnome_d - kde_d - mate_d - cinnamon]"
 fi
 
-while [ $gnome_u -ne 1 ] | [ $kde_u -ne 1 ] | [ $mate_u -ne 1 ] | [ $cinna -ne 1 ] | [ $gnome_d -ne 1 ] | [ $kde_d -ne 1 ] | [ $mate_d -ne 1 ]; do
+while [ $gnome_u -ne 1 ] | [ $kde_u -ne 1 ] | [ $mate_u -ne 1 ] | [ $cinnamon -ne 1 ] | [ $gnome_d -ne 1 ] | [ $kde_d -ne 1 ] | [ $mate_d -ne 1 ]; do
 	if [[ $envi == "gnome_u" ]]; then 
 		gnome_u=1
 		break
@@ -69,8 +73,8 @@ while [ $gnome_u -ne 1 ] | [ $kde_u -ne 1 ] | [ $mate_u -ne 1 ] | [ $cinna -ne 1
 	elif [[ $envi == "mate_u" ]]; then
        		mate_u=1
 		break			
-	elif [[ $envi == "cinna" ]]; then
-       		cinna=1
+	elif [[ $envi == "cinnamon" ]]; then
+       		cinnamon=1
 		break	
 	elif [[ $envi == "gnome_d" ]]; then
        		gnome_d=1
@@ -85,7 +89,7 @@ while [ $gnome_u -ne 1 ] | [ $kde_u -ne 1 ] | [ $mate_u -ne 1 ] | [ $cinna -ne 1
 		if [[ $envi != "null" ]];then
 			echo "Veuillez remplir correctement le champ indiqué"
 		fi
-		read dist
+		read envi
 		continue
 	fi
 done
