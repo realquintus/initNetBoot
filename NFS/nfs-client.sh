c11c417e9b2c530b4742e0f6e30cecd7ea478cf0
pablo <pablohousse@gmail.com>
2020-06-14 11:56:38 +0200

    useless commit

--- NFS/nfs-client.sh
+++ NFS/nfs-client.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 while getopts "l:p" option;do
 	case $option in
-		e)
+		l)
 			login=$OPTARG
 		;;
-		s)
+		p)
 			private_dir="true"
 		;;
 	esac
 done
 if [ -n $login ] && [ -z $private_dir ];then
 	apt-get install -y nfs-common
-	mkdir /mnt/nfs_users/
-	mount -t nfs -o sync,no_root_squash,no_subtree_check,rw 192.168.55.254:/var/nfs-server/users/ /mnt/nfs_users/
+	mkdir /mnt/nfs_users/ 2> /dev/null
+	mount -t nfs 192.168.55.254:/var/nfs-server/users/ /mnt/nfs_users/
 	echo $(ip a | grep -o "192.168.55.*/24" | cut -d "/" -f1) > /mnt/nfs_users/$login
 	echo $login > /tmp/nfs_client.tmp
 elif [ -n $private_dir ] && [ -z $login ];then
